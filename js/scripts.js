622af779512f3915f0635f5dd1311a4358c0d60c
limeoats <admin@limeoats.com>
2016-01-29 07:40:56 -0500

    announcement and analysis new blog post

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 window.posts = [
     "introductions_01-08-2016.html",
     "this-little-writers-not-so-pretty-reality_01-15-2016.html",
-    "wedding-bells-and-building-muscle_01-22-2016.html"
+    "wedding-bells-and-building-muscle_01-22-2016.html",
+    "announcement-and-analysis_01-29-2016.html"
 ];
 
 

4e97d0e83eb0c9455b2833e4b30accc29e0dfe4f
limeoats <admin@limeoats.com>
2016-04-22 13:58:06 -0400

    added blog post

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@ window.posts = [
     "recap-my-first-almost-flawless-nyfw_02-19-2016.html",
     "all-about-my-glam-brows_03-12-2016.html",
     "my-top-5-must-have-beauty-buys-for-spring-2016_03-25-2016.html",
-    "introducing-fit-and-engaged_04-08-2016.html"
+    "introducing-fit-and-engaged_04-08-2016.html",
+    "serving-up-some-positivity-with-a-side-of-protein-waffles_04-22-2016.html"
 ];
 
 
